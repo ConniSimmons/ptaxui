5ec5277b5c95ac70176314e878bcb70caf1c9f21
Conni Simmons <connisimmons@gmail.com>
2020-06-21 17:48:26 -0600

    updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React, { Component as RC, useState, useContext, useEffect } from 'react';
+import React, { Component as RC, useState, useContext, useEffect } from "react";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-    Redirect,
-} from 'react-router-dom';
-import config from './config'
-import SecureRoute from './components/SecureRoute';
-import './App.scss';
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+import config from "./config";
+import SecureRoute from "./components/SecureRoute";
+import "./App.scss";
 import Login from "./pages/Login";
-import SubmitEmail from './pages/SubmitEmail';
-import SubmitItem from './pages/SubmitItem';
-import Products from './pages/Products';
-import Home from './pages/Home';
-import { 
-  LoggedInContext, 
+import SubmitEmail from "./pages/SubmitEmail";
+import SubmitItem from "./pages/SubmitItem";
+import Products from "./pages/Products";
+import Home from "./pages/Home";
+import {
+  LoggedInContext,
   LoggedInUserContext,
   AppNameContext,
   MergedContext,
-} from './Context';
+} from "./Context";
 
 export default class App extends RC {
   constructor() {
@@ -27,72 +27,69 @@ export default class App extends RC {
     this.state = {
       loggedIn: false,
       loggedInUser: {},
-      appName: 'PTax',
+      appName: "PTax",
     };
   }
 
-   // fetch ANY model from our API
-   fetchModel = (model = '') => {
+  // fetch ANY model from our API
+  fetchModel = (model = "") => {
     // if this function is called without a valid model, return null
-    if (typeof model !== 'string' || model === '') {
-        return null;
+    if (typeof model !== "string" || model === "") {
+      return null;
     }
 
     let route = `${config.apiPath}/api/${model}`;
     fetch(route)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            // we don't know the model name so we will set it programatically
-            // create an empty object
-            const stateObj = {};
-            // set the key name using [] notation
-            stateObj[model] = data;
-            // feed that object to setState
-            this.setState(stateObj);
-        })
-        .catch((err) => {
-            console.log('Error fetching classes: ', err);
-        });
-};
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        // we don't know the model name so we will set it programatically
+        // create an empty object
+        const stateObj = {};
+        // set the key name using [] notation
+        stateObj[model] = data;
+        // feed that object to setState
+        this.setState(stateObj);
+      })
+      .catch((err) => {
+        console.log("Error fetching classes: ", err);
+      });
+  };
 
   logmein = (event) => {
     event.preventDefault();
     const [username, password] = [].slice.call(event.target.elements);
     const route = `${config.apiPath}/login`;
-    const body ={username: username.value, password: password.value};
+    const body = { username: username.value, password: password.value };
     const fetchOptions = {
       headers: {
-        'Content-Type: ': 'application.json' 
+        "Content-Type: ": "application.json",
       },
-      method: 'POST',
-      body: JSON.stringify(body)
-    }
-    fetch(route, fetchOptions)
-    .then((response) => {
-      if (response.status !== 200) {
-        return {};
-      }
-      return response.json();
-    })
-    .then((data) => {  
-      if (Object.keys(data).length)
-            {
-                this.setState({
-                    loggedIn: true,
-                    loggedInUser: data.user,
-                })
-            }
-            // need to tell the user they entered wrong username/password
-            // but i'm too lazy at the moment
-
-        })
-        .catch((err) =>
-        {
-            console.log('Error logging in...: ', err);
-        })
+      method: "POST",
+      body: JSON.stringify(body),
     };
+    fetch(route, fetchOptions)
+      .then((response) => {
+        if (response.status !== 200) {
+          return {};
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (Object.keys(data).length) {
+          this.setState({
+            loggedIn: true,
+            loggedInUser: data.user,
+          });
+        }
+        // need to tell the user they entered wrong username/password
+        // but i'm too lazy at the moment
+      })
+      .catch((err) => {
+        console.log("Error logging in...: ", err);
+      });
+  };
   logmeout = () => {
     this.setState({
       loggedIn: false,
@@ -102,34 +99,34 @@ export default class App extends RC {
   //TODO This may come in handy when the products database is functional.
   componentDidMount() {
     // get classes right at the start
-   //  this.fetchModel('classes');
+    //  this.fetchModel('classes');
     // get races right at the start
     // this.fetchModel('races');
   }
-  
 
   render() {
     return (
       <Router>
-          <Switch>
-              <MergedContext.Provider
-                  value={this.state}
-              >
-                  <AppNameContext.Provider value={this.state.appName}>
-                  <LoggedInContext.Provider value={this.state.loggedIn}>
-            <LoggedInUserContext.Provider value={this.state.loggedInUser}>
-            <SecureRoute exact path="/submititem" component={SubmitItem} />
-            //* Brian's goes to "/login" component={SubmitItem} (in case submititen doesn't work as I envision)
-            </LoggedInUserContext.Provider>
-            <Route exact path="/login" component = {Login} />
-            <Route exact path="/" component={Home} />
-            <Route exact path="/submitemail" component={SubmitEmail} />
-            <Route exact path="/products" component={Products} />
-            </LoggedInContext.Provider>
-                  </AppNameContext.Provider>
-              </MergedContext.Provider>
-          </Switch>
+        <Switch>
+          <MergedContext.Provider value={this.state}>
+            <AppNameContext.Provider value={this.state.appName}>
+              <LoggedInContext.Provider value={this.state.loggedIn}>
+                <LoggedInUserContext.Provider value={this.state.loggedInUser}>
+                  <SecureRoute
+                    exact
+                    path="/submititem"
+                    component={SubmitItem}
+                  />
+                </LoggedInUserContext.Provider>
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/" component={Home} />
+                <Route exact path="/submitemail" component={SubmitEmail} />
+                <Route exact path="/products" component={Products} />
+              </LoggedInContext.Provider>
+            </AppNameContext.Provider>
+          </MergedContext.Provider>
+        </Switch>
       </Router>
-  );
-}
+    );
+  }
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ export default class Footer extends RC {
         return(
             <footer className="main-footer">
             
-                <h6><a href="./SubmitEmail">Click HERE to be kept in the loop.</a></h6>
+                <h6><a href="./submitemail">Click HERE to be kept in the loop.</a></h6>
                 <p><small>* Site for Informational Purposes Only. *<br></br>* We do not encourage bra burning rallies in violation of social distancing recommendations. *</small></p>
               
             </footer>
